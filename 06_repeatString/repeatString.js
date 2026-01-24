a4c57197fd9eaa23e665e6d075b4d347519f11dd
mayanknaruka <mayanknaruka47@gmail.com>
2026-01-24 22:58:55 +0530

    exercise doing

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,10 +1,16 @@
 const repeatString = function (word, times) {
   if (times < 0) return "ERROR";
+
   let string = "";
   for (let i = 0; i < times; i++) {
     string += word;
   }
+
   return string;
 };
+
+// Call the function and print the result
+console.log(repeatString("hey", 3));
+
 
 module.exports = repeatString;
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    return array.filter(item => !args.includes(item))
 };
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
