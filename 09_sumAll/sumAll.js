0a862f863ce77184c7c88fbdf63bd8cee557e7dd
mayanknaruka <mayanknaruka47@gmail.com>
2026-01-25 22:40:10 +0530

    done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,12 +1,21 @@
-const sumAll = function (min, max) {
-  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
-  if (min < 0 || max < 0) return "ERROR";
-  if (min > max) {
-    const temp = min;
-    min = max;
-    max = temp;
+const sumAll = function(a, b) {
+  // Make sure a <= b
+  if (a > b) {
+    [a, b] = [b, a]; // swap
   }
-}
+
+  let sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+
+  return sum;
+};
+
+// Test
+console.log(sumAll(1, 4)); // 10
+console.log(sumAll(4, 1)); // 10 (order doesn't matter)
+
 
 // Do not edit below this line
 module.exports = sumAll;
