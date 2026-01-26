4e15aa41aa4ce0363e72bfb5ef7b83fcf35f5fb3
mayanknaruka <mayanknaruka47@gmail.com>
2026-01-26 14:48:17 +0530

    10th done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -12,10 +12,20 @@ const sumAll = function(a, b) {
   return sum;
 };
 
-// Test
-console.log(sumAll(1, 4)); // 10
-console.log(sumAll(4, 1)); // 10 (order doesn't matter)
+console.log(sumAll(1, 5)); // 10
+console.log(sumAll(4, 2)); // 10 (order doesn't matter)
 
+
+// const sumAll = function(a,b){
+//    if(a>b){
+//     [a,b] = [b,a];
+//    }
+//    let sum = 0
+//    for(let i = a; i<=b; i++ ){
+//     sum+=i
+//    }
+//    return sum 
+// }
 
 // Do not edit below this line
 module.exports = sumAll;
--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divisibleByFour = year % 4 === 0;
+    const divisibleBy100 = year % 100 === 0;
+    const divisibleBy400 = year % 400 === 0;
 
+    if (divisibleByFour && !divisibleBy100 || divisibleBy400) {
+        return true
+    }
+    else{
+        return false 
+    }
+
+    
 };
+console.log(leapYears(1990));
 
 // Do not edit below this line
 module.exports = leapYears;
