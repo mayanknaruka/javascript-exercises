9bd487db644de6ef895f30ce9ca9ddcc494c87ff
mayanknaruka <mayanknaruka47@gmail.com>
2026-05-17 13:53:20 +0530

    fibonacci series done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
 const palindromes = function (string) {
     const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789"
 
+    const cleanedString = string
+        .toLowercase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('')
+
+    const reversedString = cleanedString.split('').reverse().join('');
+
+    
+      return cleanedString === reversedString;
+
+
 };
 
 // Do not edit below this line
